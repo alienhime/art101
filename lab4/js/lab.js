4dbe0e20ee33484b22909b6e819748673d221536
Mikayla Roberts <mikayla.roberts66@gmail.com>
2021-10-13 11:38:42 -0700

    Completed Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Roshambo computer game.
+// Allows two players to participate in a virtual game of Roshambo with computer animations for each move.
+// @link   alienhime.github.io/art101/lab4
+// @file   lab.js
+// @author MikaylaRoberts
+// @since  10.13.21
+
+// Create hand sprites and animations
+// Place one hand on each side of the screen
+// Allow for user input (rock, paper, or scissors)
+// Display appropriate hand sprite
+// Calculate victor
+// Display results
+// Reset hand sprites for additional rounds
