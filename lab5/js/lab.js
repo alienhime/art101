10c04cd2fec004d16fe1b9dab497f9e943aee6c6
Mikayla Roberts <mikayla.roberts66@gmail.com>
2021-10-15 21:50:44 -0700

    Completed lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ *  Author: Mikayla Roberts <misrober@ucsc.edu>
+ *  Created: 15 October 2021
+ *  License: Public Domain
+ */
+
+// Declare all the variables you created in Task 1
+var make = "Chevrolet";
+var model = "Cruze";
+var color = "black";
+var year = 2018;
+
+// Calculate
+var age = 2021 - year;
+
+// Use document.writeln() to neatly output each of your variables
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
