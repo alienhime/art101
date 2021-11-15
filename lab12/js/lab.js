41ad4848b11431f97ee56432cba0320ac341b8ee
Mikayla Roberts <mikayla.roberts66@gmail.com>
2021-11-14 18:00:36 -0800

    added house descriptions

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -4,6 +4,12 @@
  *  License: Public Domain
  */
 
+// stores descriptions of each house
+var gryffindorDesc = '"You might belong in Gryffindor, Where dwell the brave at heart, Their daring, nerve, and chivalry Set Gryffindors apart."';
+var ravenclawDesc = '"Or yet in wise old Ravenclaw, If you\'ve a ready mind, Where those of wit and learning, Will always find their kind."';
+var slytherinDesc = '"Or perhaps in Slytherin, You\'ll make your real friends, Those cunning folk use any means, To achieve their ends"';
+var hufflepuffDesc = '"You might belong in Hufflepuff, Where they are just and loyal, Those patient Hufflepuffs are true, And unafraid of toil."';
+
 // sorts user into different houses depending on the length of their name
 function sortingHat(str) {
   var name = str;
@@ -39,4 +45,17 @@ $("button#button").click(function() {
   var house = sortingHat(name);
   // appends a new styled paragraph to #output that says "The Sorting Hat has sorted you into " + house
   $("#output").append("The Sorting Hat has sorted you into " + house + "<br></br>");
+
+  if (house == "Gryffindor") {
+    $("#output").append(gryffindorDesc + "<br></br>");
+  }
+  else if (house == "Ravenclaw") {
+    $("#output").append(ravenclawDesc + "<br></br>");
+  }
+  else if (house == "Slytherin") {
+    $("#output").append(slytherinDesc + "<br></br>");
+  }
+  else {
+    $("#output").append(hufflepuffDesc + "<br></br>");
+  }
 });
