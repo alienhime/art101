607da4e1f4b0ccc67ed710a4a60e3e44c5cd430d
Mikayla Roberts <mikayla.roberts66@gmail.com>
2021-11-17 11:53:35 -0800

    added 11 = "Bang!"

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -12,17 +12,29 @@ function fizzBuzz() {
   // Loop through numbers 1 to 200, listing them as you go
   for(var i = 1; i <= 200; i++) {
     // If the number is a multiple of more than one 3, 5 or 7, it should combine Fizz, Buzz, and Boom. For example, if the number is a multiple of 3 AND 5, it should print "FizzBuzz!" and so on.
-    if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+    if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0 && i % 11 == 0) {
+      oneLongStr += i + " FizzBuzzBoomBang!<br>";
+    }
+    else if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
       oneLongStr += i + " FizzBuzzBoom!<br>";
     }
     else if (i % 3 == 0 && i % 5 == 0) {
       oneLongStr += i + " FizzBuzz!<br>";
     }
-    else if (i % 3 == 0 && i % 7 == 0){
+    else if (i % 3 == 0 && i % 7 == 0) {
       oneLongStr += i + " FizzBoom!<br>";
     }
-    else if (i % 5 == 0 && i % 7 == 0){
+    else if (i % 3 == 0 && i % 11 == 0) {
+      oneLongStr += i + " FizzBang!<br>";
+    }
+    else if (i % 5 == 0 && i % 7 == 0) {
       oneLongStr += i + " BuzzBoom!<br>";
+    }
+    else if (i % 5 == 0 && i % 11 == 0) {
+      oneLongStr += i + " BuzzBang!<br>";
+    }
+    else if (i % 7 == 0 && i % 11 == 0) {
+      oneLongStr += i + " BoomBang!<br>";
     }
     // If the number is a multiple of 3, if should print "Fizz!"
     else if (i % 3 == 0) {
@@ -35,6 +47,10 @@ function fizzBuzz() {
     // If the number is a multiple of 7, it should print "Boom!"
     else if (i % 7 == 0) {
       oneLongStr += i + " Boom!<br>";
+    }
+    // If the number is a multiple of 11, it should print "Bang!"
+    else if (i % 11 == 0) {
+      oneLongStr += i + " Bang!<br>";
     }
     else {
       oneLongStr += i + "<br>";
